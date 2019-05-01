52f566d71f0501cac63496752d9c236af190b412
Pablo <34220081+pablogoettert@users.noreply.github.com>
2019-05-01 19:47:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function redirect_history()
+{	
+	window.location.href = ("history.html");
+}
+function redirect_history()
+{	
+	window.location.href = ("history.html");
+}
+function redirect_integ()
+{	
+	window.location.href = ("integrantes.html");
+}
+function redirect_albuns()
+{	
+	window.location.href = ("albuns.html");
+}
+function redirect_curiosidades()
+{	
+	window.location.href = ("curiosidades.html");
+}
+function home_pg()
+{	
+	window.location.href = ("home_pg.html");
+}
